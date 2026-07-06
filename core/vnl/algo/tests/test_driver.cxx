89f2e08074b61daf2930161bbe27178fa5256fd5
Insight Software Consortium Maintainers <https://discourse.itk.org/>
2026-07-06 16:14:58 -0500

    VXL 2026-07-06 (ae2f5796)
    Code extracted from:
    
        https://github.com/InsightSoftwareConsortium/vxl.git
    
    at commit ae2f5796d317ede8613a32e81369f17f83e008ee (for/itk-vxl-master-ae2f579).

--- core/vnl/algo/tests/test_driver.cxx
+++ core/vnl/algo/tests/test_driver.cxx
@@ -1,28 +1,31 @@
 #include "testlib/testlib_register.h"
 
+// VNL_EISPACK_REMOVED is defined by CMake under ITK_FUTURE_LEGACY_REMOVE, where
+// the eispack-backed vnl_*_eigensystem solvers are excluded from the build.
 DECLARE(test_amoeba);
 DECLARE(test_cholesky);
-DECLARE(test_convolve);
 DECLARE(test_determinant);
 DECLARE(test_rank);
-DECLARE(test_fft);
-DECLARE(test_fft1d);
-DECLARE(test_fft2d);
+#ifndef VNL_EISPACK_REMOVED
 DECLARE(test_generalized_eigensystem);
+#endif
 DECLARE(test_ldl_cholesky);
 DECLARE(test_levenberg_marquardt);
 DECLARE(test_matrix_update);
 DECLARE(test_minimizers);
 DECLARE(test_powell);
 DECLARE(test_qr);
-DECLARE(test_qsvd);
 DECLARE(test_rational);
+#ifndef VNL_EISPACK_REMOVED
 DECLARE(test_real_eigensystem);
+#endif
 DECLARE(test_sparse_matrix);
 DECLARE(test_integral);
 DECLARE(test_svd);
 DECLARE(test_svd_fixed);
+#ifndef VNL_EISPACK_REMOVED
 DECLARE(test_symmetric_eigensystem);
+#endif
 DECLARE(test_algo);
 DECLARE(test_solve_qp);
 DECLARE(test_sparse_lu);
@@ -35,26 +38,27 @@ register_tests()
 {
   REGISTER(test_amoeba);
   REGISTER(test_cholesky);
-  REGISTER(test_convolve);
   REGISTER(test_determinant);
   REGISTER(test_rank);
-  REGISTER(test_fft);
-  REGISTER(test_fft1d);
-  REGISTER(test_fft2d);
+#ifndef VNL_EISPACK_REMOVED
   REGISTER(test_generalized_eigensystem);
+#endif
   REGISTER(test_ldl_cholesky);
   REGISTER(test_levenberg_marquardt);
   REGISTER(test_matrix_update);
   REGISTER(test_minimizers);
   REGISTER(test_powell);
   REGISTER(test_qr);
-  REGISTER(test_qsvd);
+#ifndef VNL_EISPACK_REMOVED
   REGISTER(test_real_eigensystem);
+#endif
   REGISTER(test_integral);
   REGISTER(test_sparse_matrix);
   REGISTER(test_svd);
   REGISTER(test_svd_fixed);
+#ifndef VNL_EISPACK_REMOVED
   REGISTER(test_symmetric_eigensystem);
+#endif
   REGISTER(test_algo);
   REGISTER(test_solve_qp);
   REGISTER(test_sparse_lu);
